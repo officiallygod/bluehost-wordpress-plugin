f7b3f9185411cd7512388aafef8621c955742bed
Yashita <yashita.jain@newfold.com>
2022-08-26 17:42:52 +0530

    Added more testcases

--- tests/cypress/integration/newfold-labs/wp-module-onboarding/steps/exit-to-wordpress.cy.js
+++ tests/cypress/integration/newfold-labs/wp-module-onboarding/steps/exit-to-wordpress.cy.js
@@ -3,31 +3,50 @@
 describe('Exit to WordPress', function () {
 
     before(() => {
+        cy.setCustomerData();
         cy.visit('wp-admin/?page=nfd-onboarding&flow=ecommerce#/ecommerce/step/products');
-		cy.injectAxe();
+		// cy.injectAxe();
     });
 
+    // it('Is Accessible', () => {
+	// 	cy.checkA11y();
+	// });
 
-    it('Stay on Onboarding Page', () => {
+    it("Go to the Page where Exit to WordPress Button is visible", () => {
         var drawer_status = cy.get('.nfd-onboarding-drawer__panel-site-title-container').scrollIntoView().should('not.be.visible');
-        var noExitToWPLabel = cy.get('.is-drawer-fade > button').contains('Exit to WordPress').should('not.be.visible');
+        var noExitToWPLabel = cy.get('.nfd-onboarding-drawer__panel-back').should('not.contain', 'Exit');
         if(drawer_status || noExitToWPLabel) {
-            cy.get('button').contains('Next').click();
+            cy.get('.navigation-buttons_next').click();
         }
-        
-        cy.get('.is-drawer-fade > button').contains('Exit to WordPress').click();
-        cy.get('.components-modal__header > button').click();
+        cy.url().should('not.contain', '/ecommerce/step/products');
+        cy.get('.nfd-onboarding-etw__trigger').click();
+        cy.get('.components-modal__content').should('be.visible');
+    });
 
-        cy.get('.is-drawer-fade > button').contains('Exit to WordPress').click();
-        cy.get('.is-secondary').contains('Continue').click();
+    it('Check if heading and paragh content exists', () => {
+        cy.get('h1.components-modal__header-heading').should('be.visible');
+        cy.get('.components-modal__content > p').should('be.visible');
+    });
 
-        cy.url().should('contain', '#/wp-setup/step/basic-info');
+    it("Stay on Onboarding Page when 'X'/Continue is clicked", () => {
+        var currUrl = cy.url().should('not.contain', '/ecommerce/step/products');
+
+        cy.get('.components-modal__header > .components-button').click();
+        currUrl;
+
+        cy.get('.nfd-onboarding-etw__trigger').click();
+        cy.get('.nfd-onboarding-etw__buttons > .is-secondary').click();
+        currUrl;
     });
 
     it('Exit to WordPress Page', () => {
-        cy.get('.is-drawer-fade > button').contains('Exit to WordPress').click();
-        cy.get('.is-primary').contains('Exit').click();
-        cy.wait(3000);
+        cy.get('.nfd-onboarding-etw__trigger').click();
+        cy.get('.nfd-onboarding-etw__buttons > .is-primary').click();
+        cy.wait(5000);
         cy.url().should('contain', '#/home/onboarding');
+    });
+
+    after(() => {
+        cy.clearCustomerData();
     });
 });
--- tests/cypress/integration/newfold-labs/wp-module-onboarding/steps/get-started-experience.cy.js
+++ tests/cypress/integration/newfold-labs/wp-module-onboarding/steps/get-started-experience.cy.js
@@ -3,29 +3,32 @@
 describe('Start Setup WP Experience Page', function () {
 
 	before(() => {
+        cy.setCustomerData();
 		cy.visit('wp-admin/?page=nfd-onboarding&flow=ecommerce#/wp-setup/step/get-started/experience');
-		cy.injectAxe();
+		// cy.injectAxe();
 	});
 
-    it('Check if Drawer Suppressed', () => {
-        cy.get('.nfd-onboarding-drawer__panel-site-title-container').scrollIntoView().should('not.be.visible');
+    // it('Is Accessible', () => {
+	// 	cy.checkA11y();
+	// });
+
+    it('Check if the Suppressed Drawer does not open on clicking Toggle Button', () => {
+        cy.wait(3000);
+        cy.get('.nfd-onboarding-drawer__toggle-button').click().and('have.class','is-suppressed');
+        cy.get('.nfd-onboarding-drawer__panel-scroll-container').scrollIntoView().should('not.be.visible');
     });
 
     it('Check if Headers Load', () => {
-        cy.get('.nfd-step-card-heading').should('exist');
-        cy.get('.nfd-step-card-subheading-other').should('exist');
-        cy.get('.nfd-step-card-question').should('exist');
+        cy.get('.nfd-step-card-heading').should('exist').and('be.visible');
+        cy.get('.nfd-step-card-subheading-other').should('exist').and('be.visible');
+        cy.get('.nfd-step-card-question').should('exist').and('be.visible');
     });
 
     it('Check if radio options load', () => {
-        cy.get('.components-radio-control__option').should('exist');
-    });
-
-    it('Navigation Buttons Landing on expected pages', () => {
-        cy.get('button').contains('Back').click();
-        cy.url().should('contain', 'get-started/welcome');
-        cy.get('button').contains('Next').click();
-        cy.url().should('contain', 'get-started/experience');
+        cy.get('.components-radio-control__option')
+        .should('exist')
+        .and('be.visible')
+        .and('have.length', 3);
     });
 
     it('Enable Button based on the radio button selected', () => {
@@ -34,11 +37,34 @@ describe('Start Setup WP Experience Page', function () {
 
         cy.get('[type="radio"]').check('1', { force: true });
         cy.get('[type=radio]:checked').should('have.css', 'background-color', 'rgb(53, 117, 211)');
-        cy.get('.nfd-card-button').should('not.be.disabled').contains('Continue Setup').click();
-        cy.url().should('contain', 'get-started/site-secondary');
+        cy.get('.nfd-card-button').should('not.be.disabled').click();
+        cy.url().should('not.contain', 'get-started/experience');
+        cy.go('back');
     });
 
-    it('Check Need Help Tag', () => {
+    it('Checks if all the radio control buttons are enabled', () => {
+        cy.get('.components-radio-control__option')
+        .each(($radioControl) => {
+            cy.wrap($radioControl).find('input').should('not.be.disabled');
+        });
+    });
+
+    it('Navigation Buttons Landing on expected pages', () => {
+        cy.get('.navigation-buttons_next').click();
+        cy.url().should('not.include', '/get-started/experience');
+        cy.go('back');
+
+        cy.get('.navigation-buttons_back').click();
+        cy.url().should('not.include', '/get-started/experience');
+        cy.go('back');
+    });
+
+    it('Check Need Help Tag and Hire Experts URL', () => {
+        cy.get('.nfd-card-need-help-tag').scrollIntoView().should('be.visible');
         cy.get('.nfd-card-need-help-tag > a').should('exist').should('have.attr', 'href');
+    });
+
+    after(() => {
+        cy.clearCustomerData();
     });
 });
