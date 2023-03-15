c09926801dc9f8a87d37e5b501a6ceac5485f4d5
Allen Benny <48691514+officiallygod@users.noreply.github.com>
2023-03-15 12:09:30 +0530

    Rename to run tests

--- tests/cypress/integration/newfold-labs/wp-module-onboarding/steps/get-started-experiences.cy.js
+++ tests/cypress/integration/newfold-labs/wp-module-onboarding/steps/get-started-experiences.cy.js
@@ -18,7 +18,7 @@ describe( 'Start Setup WP Experience Page', function () {
 		);
 	} );
 
-	it( 'Check if the Suppressed Drawer does not open on clicking Toggle Button.', () => {
+	it( 'Check if the Suppressed Drawer does not open on clicking Toggle Button', () => {
 		CheckDrawerDisabled();
 	} );
 
